efa9ddd495ce7c1519883559abbc4d4f79df04c8
RicardoHarrer <harrer.r20@htlwienwest.at>
2026-01-15 12:13:10 +0100

    touch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   public float playerSpeed = 5;
-   public float horitontalSpeed = 5;
-   public float rightLimit = 3.5f;
-   public float leftLimit = -3.5f;
+    public float playerSpeed = 5f;
+    public float horizontalSpeed = 5f;
+    public float rightLimit = 3.5f;
+    public float leftLimit = -3.5f;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
-        if (Input.GetKey(KeyCode.A))
-        {
-            if (this.gameObject.transform.position.x > leftLimit)
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * horitontalSpeed);
-            }
+        // Forward movement
+        transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime, Space.World);
 
-        }
-        if (Input.GetKey(KeyCode.D))
+#if UNITY_EDITOR || UNITY_STANDALONE
+        // PC Controls
+        if (Input.GetKey(KeyCode.A) && transform.position.x > leftLimit)
         {
-            if (this.gameObject.transform.position.x < rightLimit)
+            MoveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x < rightLimit)
+        {
+            MoveRight();
+        }
+#else
+        // Mobile Controls
+        HandleTouchInput();
+#endif
+    }
+
+    void HandleTouchInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.position.x < Screen.width / 2 && transform.position.x > leftLimit)
             {
-                transform.Translate(Vector3.left * Time.deltaTime * horitontalSpeed * -1);
+                MoveLeft();
+            }
+            else if (touch.position.x > Screen.width / 2 && transform.position.x < rightLimit)
+            {
+                MoveRight();
             }
         }
     }
-}
 
+    void MoveLeft()
+    {
+        transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+    }
+
+    void MoveRight()
+    {
+        transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+    }
+}
